8029800d48937a35619e01ac09721ebf8276f5ae
Meagan Utsey <meagan.utsey@gmail.com>
2020-01-04 19:58:30 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -16,3 +16,22 @@ class Artist
   end
 end
 
+class Song
+  attr_accessor :artist, :name, :genre
+ 
+  @@all = []
+ 
+  def initialize(name, genre)
+    @name = name
+    @genre = genre
+    save
+  end
+ 
+  def save
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+end
